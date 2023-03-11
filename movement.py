b1ae7cc82a82425b3a6c51c517073df9ee2c8d7e
R0ttenr0b <aragoro86@gmail.com>
2023-03-10 21:01:10 -0800

    added player and map

---
+++ movement.py
@@ -0,0 +1,13 @@
+pla_x = 1
+pla_y = 1
+pla_dir = 'up'
+# where the player is starting 
+
+#making a 5x5 map
+map = [
+    [1, 1, 1, 1, 1]
+    [1, 1, 1, 1, 1]
+    [1, 1, 1, 1, 1]
+    [1, 1, 1, 1, 1]
+    [1, 1, 1, 1, 1]
+]
