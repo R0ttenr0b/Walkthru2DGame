3d51791e06ef04833478f03281308c3f79d3b20e
R0ttenr0b <aragoro86@gmail.com>
2023-03-11 16:26:17 -0800

    working on the user to move

--- movement.py
+++ movement.py
@@ -7,10 +7,10 @@ pla_dir = 'up'
 
 #making a 5x5 map
 map = [
-    [1, 1, 1, 1, 1]
-    [1, 1, 1, 1, 1]
-    [1, 1, 1, 1, 1]
-    [1, 1, 1, 1, 1]
+    [1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1],
     [1, 1, 1, 1, 1]
 ]
 #images
@@ -19,7 +19,7 @@ for i in range(32):
 
 #move function
 def move(direction):
-    global pla_pos
+    global pla_x pla_y
 
     new_pos = pla_pos.copy()
     if direction == "left":
@@ -35,3 +35,7 @@ def move(direction):
 direction = input("move (left,up, right, down);")
 
 move(direction)
+
+
+while True:
+ print("Player position:",pla_y,pla_x)
