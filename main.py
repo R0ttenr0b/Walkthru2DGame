2c7b2d638db595ff438a752cfe4b62fca8bcd112
R0ttenr0b <124658302+R0ttenr0b@users.noreply.github.com>
2023-03-17 16:35:02 -0700

    changed some my file paths to call the image needed

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 import json
 
+
+
 # where the player is starting 
 play_pos = [0,0]
 
@@ -26,15 +28,42 @@ def move(direction):
 
     new_pos = play_pos.copy()
     if direction == "left":
-        new_pos [1] =- 1
+        new_pos [1] -= 1
     elif direction == "right":
-        new_pos [1] =+ 1
+        new_pos [1] += 1
     elif direction == "up":
-        new_pos [0] =- 1
+        new_pos [0] -= 1
     elif direction == "down":
-        new_pos [0] =+ 1
+        new_pos [0] += 1
+
+    if 0 <= new_pos[0] < len(map) and 0 <= new_pos[1] < len(map[0]):
+        tile = map[new_pos[0]][new_pos[1]]
+        if tile != (0, 0):
+            play_pos = new_pos
 
 #get the user to move
 direction = input("move (left,up, right, down):")
 
 move(direction)
+# display game map with player position
+for i, row in enumerate(map):
+    for j, tile in enumerate(row):
+        if (i, j) == tuple(play_pos):
+            # display player image
+            print(images['player'], end=' ')
+        elif tile == (0, 0):
+            # display empty tile image
+            print(images['empty'], end=' ')
+        elif tile == (1, 0):
+            # display left wall image
+            print(images['left_wall'], end=' ')
+        elif tile == (2, 0):
+            # display right wall image
+            print(images['right_wall'], end=' ')
+        elif tile == (3, 0):
+            # display top wall image
+            print(images['top_wall'], end=' ')
+        elif tile == (4, 0):
+            # display bottom wall image
+            print(images['bottom_wall'], end=' ')
+    print()
