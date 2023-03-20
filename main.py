c6d19886f899aeaf25429b4bce93f56836c387ec
R0ttenr0b <124658302+R0ttenr0b@users.noreply.github.com>
2023-03-19 18:42:26 -0700

    added the img,user,imageind and display

--- main.py
+++ main.py
@@ -24,9 +24,28 @@ current_image_index = 0
 
 def next_image_index(directiuon):
     global current_image_index
-    current_image = image[play_pos[0]][play_pos[1]]
+    current_image = images[play_pos[0]][play_pos[1]]
     next_image_pos = current_image["next"].get(directiuon)
     if next_image_pos is not None:
-        next_image = image[current_image]
+        next_image = images[next_image_pos[0]][play_pos[1]]
+        current_image_index = images.index(next_image)
+        return current_image_index
+    
+#display the images
+def display_image():
+    current_image = images[current_image_index]
+     filename = current_image["filename"]
+  image_path = os.path.join("images", filename)
+    # display image here
+    print("Displaying image:", image_path)
+
+# loop for user input and image display
+while True:
+    # get user input
+    direction = input("move (left,up, right, down):")
+    # get next image index based on user input
+    current_image_index = get_next_image_index(direction)
+    # display current image
+    display_image()
 
         
