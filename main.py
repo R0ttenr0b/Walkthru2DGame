3a32302eeb18dadd9882eb801f3807aa2026d043
R0ttenr0b <124658302+R0ttenr0b@users.noreply.github.com>
2023-03-20 19:29:12 -0700

    changed main and jdon

--- main.py
+++ main.py
@@ -1,39 +1,41 @@
 import os
 import json
 
-#making a 5x5 map
+# define map
 map = [
-    [(1,0), (0, 0), (0, 0), (0, 0), (0, 2)],
-    [(0,0), (0, 0), (0, 0), (0, 0), (0, 0)],
-    [(0,0), (0, 0), (0, 0), (0, 0), (0, 0)],
-    [(0,0), (0, 0), (0, 0), (0, 0), (0, 0)],
-    [(3,0), (0, 0), (0, 0), (0, 0), (0, 4)], ]
- 
-# where the player is starting 
-play_pos = [0,0]   
+    [(1, 0), (0, 0), (0, 0), (0, 0), (0, 2)],
+    [(0, 0), (0, 0), (0, 0), (0, 0), (0, 0)],
+    [(0, 0), (0, 0), (0, 0), (0, 0), (0, 0)],
+    [(0, 0), (0, 0), (0, 0), (0, 0), (0, 0)],
+    [(3, 0), (0, 0), (0, 0), (0, 0), (0, 4)]
+]
 
-#images
-with open("2dpics.json","r") as f:
+# define images
+with open("2dpics.json", "r") as f:
     images_data = json.load(f)
     images = images_data.get("images", [])
 
-current_image_index = images.index(images[play_pos[0]][play_pos[1]])
+# define player starting position and current image
+play_pos = [0, 0]
+current_image = images[play_pos[0]][play_pos[1]]["front"]
 
+# define function to get next image index
 def next_image_index(direction):
-    global current_image_index
+    global current_image
     global play_pos
-    current_image = images[play_pos[0]][play_pos[1]]
-    next_image_pos = current_image["next"].get(direction)
-    if next_image_pos is not None:
-        next_image = images[next_image_pos[0]][play_pos[1]]
-        current_image_index = images.index(next_image)
-        play_pos = next_image_pos
-        return current_image_index
-    
-#display the images
+    current_position = images[play_pos[0]][play_pos[1]]
+    next_position = current_position.get(direction)
+    if next_position is not None:
+        next_position_x, next_position_y = next_position
+        next_position_image = images[next_position_x][next_position_y]
+        current_image = next_position_image["front"]
+        play_pos = next_position
+        return current_image
+
+# define function to display current image
 def display_image():
-    current_image = images[play_pos[0]][play_pos[1]]
-    filename = current_image["filename"]
+    current_position = images[play_pos[0]][play_pos[1]]
+    filename = current_position["front"]
     image_path = os.path.join("images", filename)
     # display image here
     print("Displaying image:", image_path)
@@ -43,8 +45,9 @@ while True:
     # get user input
     direction = input("move (left,up, right, down):")
     # get next image index based on user input
-    current_image_index = next_image_index(direction)
-    # display current image
-    display_image()
-
-        
+    next_image = next_image_index(direction)
+    if next_image is None:
+        print("Cannot move in that direction")
+    else:
+        # display current image
+        display_image()
